08f8ceb743ac0b9d5e0cd53aa50ef235e6797f76
Mohamed Faiz <220701167@rajalakshmi.edu.in>
2025-03-25 09:13:30 +0530

    Delete src directory

--- src/components/AlertsReports.jsx
+++
@@ -1,7 +0,0 @@
-const AlertsReports = () => {
-    return (
-        <div className="text-3xl font-bold tracking-tight primary-text-gradient">AlertsReports</div>
-    )
-}
-
-export default AlertsReports
